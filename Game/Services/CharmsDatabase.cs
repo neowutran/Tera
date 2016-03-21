f56080236da3664fcf41459af360a24bd1a6fd70
Gl0 <ab@glk.spb.ru>
2016-03-21 23:47:55 +0300

    Add charms database

---
+++ Game/Services/CharmsDatabase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Tera.Game.Messages;
+
+namespace Tera.Game
+{
+    // Contains information about skills
+    // Currently this is limited to the name of the skill
+    public class CharmsDatabase
+    {
+        private readonly Dictionary<uint, string> _charms = new Dictionary <uint, string>();
+
+        public CharmsDatabase(string directory, string reg_lang)
+        {
+            var lines = File.ReadLines(Path.Combine(directory, $"hotdot\\charms-{reg_lang}.tsv"));
+            var listOfParts = lines.Select(s => s.Split(new[] { '\t' }, 2));
+            foreach (var parts in listOfParts)
+            {
+                _charms.Add(uint.Parse(parts[0]), parts[1]);
+            }
+        }
+
+        public string GetCharmName(uint charmId)
+        {
+            string result = string.Empty;
+            _charms.TryGetValue(charmId, out result);
+            return result;
+        }
+    }
+}
