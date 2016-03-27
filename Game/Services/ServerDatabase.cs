d83647d9c9ce61add11fbdf3dfcb7c3bad16cc87
Gl0 <ab@glk.spb.ru>
2016-03-27 22:45:07 +0300

    fix server overrides

--- Game/Services/ServerDatabase.cs
+++ Game/Services/ServerDatabase.cs
@@ -16,7 +16,7 @@ namespace Tera.Game
             _serverlist = File.ReadAllLines(Path.Combine(folder, $"servers.txt"))
                 .Where(s => !string.IsNullOrWhiteSpace(s))
                 .Select(s => s.Split(new[] { ' ' }, 4))
-                .Select(parts => new Server(parts[3], parts[1], parts[0], !string.IsNullOrEmpty(parts[2])?uint.Parse(parts[2]):uint.MaxValue));
+                .Select(parts => new Server(parts[3], parts[1], parts[0], !string.IsNullOrEmpty(parts[2])?uint.Parse(parts[2]):uint.MaxValue)).ToList();
             _servers =_serverlist.Where(x=>x.ServerId!=uint.MaxValue).ToDictionary(x=>Tuple.Create(x.ServerId,x.Region));
         }
 
@@ -31,7 +31,7 @@ namespace Tera.Game
         }
         public void AddOverrides(IEnumerable<Server> newServers)
         {
-            _serverlist = _serverlist.Concat(newServers.Where(sl => newServers.All(os => os.Ip != sl.Ip)));
+            _serverlist = _serverlist.Concat(newServers.Where(sl => _serverlist.All(os => os.Ip != sl.Ip))).ToList();
         }
         public Server GetServer(uint serverId, Server oldServer = null)
         {
