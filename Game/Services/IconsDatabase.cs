7a0c5f74738aa3f23073347cf98f7054d53d669d
Gl0 <ab@glk.spb.ru>
2016-04-16 01:57:00 +0300

    Add Bitmap support for icons database

--- Game/Services/IconsDatabase.cs
+++ Game/Services/IconsDatabase.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.IO;
 using System.IO.Packaging;
 using System.Windows.Media.Imaging;
+using System.Drawing;
+
 namespace Tera.Game
 {
     public class IconsDatabase
     {
         private readonly Dictionary<string, BitmapImage> _images = new Dictionary<string, BitmapImage>();
-//        private readonly string IconsDirectory;
+        private readonly Dictionary<string, Bitmap> _bitmaps = new Dictionary<string, Bitmap>();
         private readonly Package _icons;
         public IconsDatabase(string ResourceDirectory)
         {
@@ -35,6 +37,19 @@ namespace Tera.Game
             //var filename = IconsDirectory + iconName + ".png";
             //image = new BitmapImage(new Uri(filename));
             _images.Add(iconName, image);
+            return image;
+        }
+        public Bitmap GetBitmap(string iconName)
+        {
+            Bitmap image;
+            if (_bitmaps.TryGetValue(iconName, out image) || string.IsNullOrEmpty(iconName))
+            {
+                return image;
+            }
+            Uri ur = new Uri("/" + iconName + ".png", UriKind.Relative);
+            if (_icons.PartExists(ur)) image = new Bitmap(_icons.GetPart(ur).GetStream());
+            else image = new Bitmap(1, 1);
+            _bitmaps.Add(iconName, image);
             return image;
         }
     }
