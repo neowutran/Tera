219d0f3b682226cea2a49ace06f070d6fdcc8b78
Gl0 <ab@glk.spb.ru>
2016-04-22 17:39:07 +0300

    Remove region from server lookup by id

--- Game/Services/ServerDatabase.cs
+++ Game/Services/ServerDatabase.cs
@@ -7,7 +7,7 @@ namespace Tera.Game
 {
     public class ServerDatabase
     {
-        private readonly Dictionary<Tuple<uint,string>, Server> _servers = new Dictionary<Tuple<uint, string>, Server>();
+        private readonly Dictionary<uint, Server> _servers = new Dictionary<uint, Server>();
         private IEnumerable<Server> _serverlist = new List<Server>();
         public string Region { get; set; }
 
@@ -17,13 +17,12 @@ namespace Tera.Game
                 .Where(s => !string.IsNullOrWhiteSpace(s))
                 .Select(s => s.Split(new[] { ' ' }, 4))
                 .Select(parts => new Server(parts[3], parts[1], parts[0], !string.IsNullOrEmpty(parts[2])?uint.Parse(parts[2]):uint.MaxValue)).ToList();
-            _servers =_serverlist.Where(x=>x.ServerId!=uint.MaxValue).ToDictionary(x=>Tuple.Create(x.ServerId,x.Region));
+            _servers =_serverlist.Where(x=>x.ServerId!=uint.MaxValue).ToDictionary(x=>x.ServerId);
         }
 
-        public string GetServerName(uint serverId, Server oldServer = null) // not sure, whether Servers in different regions have unique ServerId's, if so - delete Tuple and lookup only by ServerId
+        public string GetServerName(uint serverId, Server oldServer = null)
         {
-            var key = Tuple.Create(serverId, Region);
-            return _servers.ContainsKey(key) ? _servers[key].Name : oldServer?.Name ?? $"{Region}: {serverId.ToString()}";
+            return _servers.ContainsKey(serverId) ? _servers[serverId].Name : oldServer?.Name ?? $"{serverId.ToString()}";
         }
         public Dictionary<string,Server> GetServersByIp()
         {
@@ -35,8 +34,7 @@ namespace Tera.Game
         }
         public Server GetServer(uint serverId, Server oldServer = null)
         {
-            var key = Tuple.Create(serverId, Region);
-            return _servers.ContainsKey(key) ? _servers[key] : oldServer;
+            return _servers.ContainsKey(serverId) ? _servers[serverId] : oldServer;
         }
     }
 }
