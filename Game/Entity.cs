15e809768c9ff98a8734024addbbffab5e181976
neowutran <me@neowutran.ovh>
2016-06-21 13:20:21 +0200

    entity equals

--- Game/Entity.cs
+++ Game/Entity.cs
@@ -1,8 +1,51 @@
-﻿namespace Tera.Game
+﻿using System;
+
+namespace Tera.Game
 {
     // An object with an Id that can be spawned or deswpawned in the game world
-    public class Entity
+    public class Entity : IEquatable<object>
     {
+
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj.GetType() == GetType() && Equals((Entity)obj);
+        }
+
+        public bool Equals(Entity other)
+        {
+            return Id == other.Id;
+        }
+
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if (ReferenceEquals(a, b))
+            {
+                return true;
+            }
+
+            // If one is null, but not both, return false.
+            if (((object)a == null) || ((object)b == null))
+            {
+                return false;
+            }
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+
         public Entity(EntityId id)
         {
             Id = id;
