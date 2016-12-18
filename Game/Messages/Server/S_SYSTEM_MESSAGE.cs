7dfd3537c4b5aa3f668b05a26e8d8eb75158f60f
neowutran <me@neowutran.ovh>
2016-12-18 18:59:06 +0100

    add system message

---
+++ Game/Messages/Server/S_SYSTEM_MESSAGE.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Tera.Game.Messages
+{
+    public class S_SYSTEM_MESSAGE : ParsedMessage
+    {
+        internal S_SYSTEM_MESSAGE(TeraMessageReader reader) : base(reader)
+        {
+           // PrintRaw();
+        }
+
+    }
+}
