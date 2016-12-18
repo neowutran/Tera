bcfdfda7e0275e16ad4baeec857fe7e38eb00433
Gl0 <ab@glk.spb.ru>
2016-12-19 01:43:58 +0300

    Add vanguard credits tracking messages

---
+++ Game/Messages/Server/S_AVAILABLE_EVENT_MATCHING_LIST.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace Tera.Game.Messages
+{
+    public class S_AVAILABLE_EVENT_MATCHING_LIST : ParsedMessage
+
+    {
+        internal S_AVAILABLE_EVENT_MATCHING_LIST(TeraMessageReader reader) : base(reader)
+        {
+            reader.Skip(24);
+            Badges = reader.ReadInt32();
+            Credits = reader.ReadInt32();
+            Debug.WriteLine(Type+" Badges:"+Badges+" Credits:"+Credits);
+        }
+
+        public int Badges { get; private set; }
+        public int Credits { get; private set; }
+        public static S_UPDATE_NPCGUILD.NpcGuildType Type = S_UPDATE_NPCGUILD.NpcGuildType.Vanguard;
+    }
+}
---
+++ Game/Messages/Server/S_UPDATE_NPCGUILD.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+namespace Tera.Game.Messages
+{
+    public class S_UPDATE_NPCGUILD : ParsedMessage
+    {
+        public enum NpcGuildType
+        {
+		    Vanguard=609
+        }
+
+        internal S_UPDATE_NPCGUILD(TeraMessageReader reader) : base(reader)
+        {
+            User=reader.ReadEntityId();
+            reader.Skip(8);
+            int type = reader.ReadInt32();
+            Type = (NpcGuildType)type;
+            reader.Skip(8);
+            Credits = reader.ReadInt32();
+            Debug.WriteLine("type:"+type+";translated:"+Type + "; Credits:"+Credits);
+        }
+
+        public EntityId User { get; private set; }
+        public int Credits { get; private set; }
+        public NpcGuildType Type { get; private set; }
+    }
+}
+
