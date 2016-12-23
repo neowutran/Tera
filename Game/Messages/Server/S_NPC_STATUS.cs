a4735acbbaf083e1e524bb465e719c4f8b39e9e3
Gl0 <ab@glk.spb.ru>
2016-12-23 07:49:37 +0300

    Add SNpcStatus.Defeated ?

--- Game/Messages/Server/S_NPC_STATUS.cs
+++ Game/Messages/Server/S_NPC_STATUS.cs
@@ -1,4 +1,6 @@
-﻿namespace Tera.Game.Messages
+﻿using System.Diagnostics;
+
+namespace Tera.Game.Messages
 {
     public class SNpcStatus : ParsedMessage
 
@@ -7,13 +9,14 @@
         {
             Npc = reader.ReadEntityId();
             Enraged = (reader.ReadByte() & 1) == 1;
-            reader.Skip(4);
+            Defeated = reader.ReadInt32()==5;//maybe, need test
             Target = reader.ReadEntityId();
             //Debug.WriteLine("NPC:" + Npc + ";Target:" + Target + (Enraged?" Enraged":""));
         }
 
         public EntityId Npc { get; }
         public bool Enraged { get; }
+        public bool Defeated { get; }
         public EntityId Target { get; }
     }
 }
