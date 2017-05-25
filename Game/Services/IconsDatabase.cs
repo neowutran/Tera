f686df12e4043212f55899d8fc754119cf9292d0
Gl0 <Gl0@users.noreply.github.com>
2017-05-25 18:46:58 +0300

    fix uninitialized bitmapimage error (don't see other places where it can come from)

--- Game/Services/IconsDatabase.cs
+++ Game/Services/IconsDatabase.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.IO.Packaging;
 using System.Windows.Media.Imaging;
@@ -12,11 +13,23 @@ namespace Tera.Game
         private readonly Dictionary<string, Bitmap> _bitmaps = new Dictionary<string, Bitmap>();
         private readonly Package _icons;
         private readonly Dictionary<string, BitmapImage> _images = new Dictionary<string, BitmapImage>();
+        private readonly BitmapImage _emptyBitmap;
 
         public IconsDatabase(string resourceDirectory)
         {
 //            IconsDirectory = Path.Combine(resourceDirectory, "icons/");
             _icons = Package.Open(resourceDirectory + "icons.zip", FileMode.Open, FileAccess.Read, FileShare.Read);
+            using (MemoryStream memory = new MemoryStream())
+            {
+                (new Bitmap(1,1)).Save(memory,ImageFormat.Bmp);
+                memory.Position = 0;
+                _emptyBitmap = new BitmapImage();
+                _emptyBitmap.BeginInit();
+                _emptyBitmap.StreamSource = memory;
+                _emptyBitmap.CacheOption=BitmapCacheOption.OnLoad;
+                _emptyBitmap.EndInit();
+                _emptyBitmap.Freeze();
+            }
         }
 
         public BitmapImage GetImage(string iconName)
@@ -34,7 +47,9 @@ namespace Tera.Game
                 image.CacheOption = BitmapCacheOption.OnLoad;
                 image.StreamSource = _icons.GetPart(ur).GetStream();
                 image.EndInit();
+                image.Freeze();
             }
+            else { image = _emptyBitmap; }
             //var filename = IconsDirectory + iconName + ".png";
             //image = new BitmapImage(new Uri(filename));
             _images.Add(iconName, image);
