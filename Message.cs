d44652d648a37637646bb220604b40be42a1e671
Neowutran <me@neowutran.ovh>
2017-08-31 17:32:03 +0200

    ./Message.cs

--- Message.cs
+++ Message.cs
@@ -13,7 +13,7 @@ namespace Tera
 
         public DateTime Time { get; private set; }
         public MessageDirection Direction { get; private set; }
-        public ArraySegment<byte> Data { get; }
+        public ArraySegment<byte> Data { get; set; }
 
         public ushort OpCode => (ushort) (Data.Array[Data.Offset] | Data.Array[Data.Offset + 1] << 8);
         public ArraySegment<byte> Payload => new ArraySegment<byte>(Data.Array, Data.Offset + 2, Data.Count -2);
