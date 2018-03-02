db21e136629b39647eb36788a52142d4842fa9e8
Gl0 <Gl0@users.noreply.github.com>
2018-03-02 16:30:07 +0300

    BHS version numbers are hard to predict... fix https://github.com/neowutran/ShinraMeter/issues/414

--- Game/Messages/Server/SpawnNpcServerMessage.cs
+++ Game/Messages/Server/SpawnNpcServerMessage.cs
@@ -17,7 +17,7 @@ namespace Tera.Game.Messages
             NpcId = reader.ReadUInt32();
             NpcArea = reader.ReadUInt16();
             CategoryId = reader.ReadUInt32();
-            reader.Skip(reader.Version >= 325714 && reader.Version <326430 ? 35 : 31);//KR added 4 bytes somewere inside
+            reader.Skip(reader.Version >= 328305 ? 35 : 31);//KR added 4 bytes somewere inside
             OwnerId = reader.ReadEntityId();
         }
 
