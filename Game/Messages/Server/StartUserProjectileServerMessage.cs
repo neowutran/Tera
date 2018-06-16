48ff3e1158c94f4369664247fedf72f49dcf9aa9
Gl0 <Gl0@users.noreply.github.com>
2018-06-16 04:36:08 +0300

    S_START_USER_PROJECTILE SkillId => ActionId

--- Game/Messages/Server/StartUserProjectileServerMessage.cs
+++ Game/Messages/Server/StartUserProjectileServerMessage.cs
@@ -8,17 +8,17 @@
             OwnerId = reader.ReadEntityId();
             reader.Skip(reader.Factory.ReleaseVersion>=7401?8:4);// something added
             Id = reader.ReadEntityId();
-            SkillId = new SkillId(reader).Id;
+            ActionId = reader.ReadUInt32();
             Start = reader.ReadVector3f();
             Finish = reader.ReadVector3f();
             Speed = reader.ReadSingle();
-            //Debug.WriteLine($"{Time.Ticks} {BitConverter.ToString(BitConverter.GetBytes(Id.Id))} {Start} - > {Finish} {Speed} {SkillId}");
+            //Debug.WriteLine($"{Time.Ticks} {BitConverter.ToString(BitConverter.GetBytes(Id.Id))} {Start} - > {Finish} {Speed} {ActionId}");
         }
 
         public float Speed { get; set; }
         public Vector3f Finish { get; set; }
         public Vector3f Start { get; set; }
-        public int SkillId { get; set; }
+        public uint ActionId { get; set; }
         public EntityId Id { get; private set; }
         public EntityId OwnerId { get; private set; }
     }
