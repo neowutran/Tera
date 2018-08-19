2a53ec880647c0fb01c893d1e4f89ebae06aab8a
Gl0 <Gl0@users.noreply.github.com>
2018-08-19 04:27:12 +0300

    fix S_GET_USER_LIST for JP-Classic (hope all is ok, but may be some other things changed)

--- Game/Messages/Server/S_GET_USER_LIST.cs
+++ Game/Messages/Server/S_GET_USER_LIST.cs
@@ -19,8 +19,8 @@ namespace Tera.Game.Messages
                 reader.Skip(14);
                 var gNameOffset = reader.ReadUInt16();
                 var playerId = reader.ReadUInt32();
-                reader.Skip(294);
-                reader.Skip(121);//added accessory transformation
+                reader.Skip(reader.Factory.ReleaseVersion < 6200 ? 286 : 294);
+                if (reader.Factory.ReleaseVersion >= 6603) reader.Skip(121);//added accessory transformation
                 var guildId = reader.ReadUInt32();
                 reader.BaseStream.Position = gNameOffset - 4;
                 var gName = reader.ReadTeraString();
