42b992f1cb0800fb62aa9932ea784685d222f94c
Gl0 <Gl0@users.noreply.github.com>
2019-02-12 17:05:06 +0300

    cpoy paste is so copy paste...

--- Game/Services/SkillDatabase.cs
+++ Game/Services/SkillDatabase.cs
@@ -19,7 +19,6 @@ namespace Tera.Game
         public SkillDatabase(string directory, string reg_lang)
         {
             try {InitializeSkillDatabase(Path.Combine(directory, $"skills\\skills-override-{reg_lang}.tsv"));} catch {}
-            InitializeSkillDatabase(Path.Combine(directory, $"skills\\skills-override-{reg_lang}.tsv"));
             InitializeSkillDatabase(Path.Combine(directory, $"skills\\skills-{reg_lang}.tsv"));
             var gunner = new RaceGenderClass(Race.Common, Gender.Common, PlayerClass.Gunner);
             for (int j = 0; j <= 2; j++) {
