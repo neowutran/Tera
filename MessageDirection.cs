bb87f0f2778819bc4769ff7a7dbfafaa087dfd67
Foglio1024 <foglio1024@gmail.com>
2020-12-07 20:41:06 +0100

    implement toolbox stuff

--- MessageDirection.cs
+++ MessageDirection.cs
@@ -3,6 +3,7 @@
     public enum MessageDirection
     {
         ClientToServer = 1,
-        ServerToClient = 2
+        ServerToClient = 2,
+        Unspecified = 100
     }
 }
--- Sniffing/ITeraSniffer.cs
+++ Sniffing/ITeraSniffer.cs
@@ -2,6 +2,8 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using Tera.Game;
 
 namespace Tera.Sniffing
@@ -11,5 +13,14 @@ namespace Tera.Sniffing
         bool Enabled { get; set; }
         event Action<Message> MessageReceived;
         event Action<Server> NewConnection;
+
+        event Action EndConnection;
+        bool EnableMessageStorage { get; set; }
+        ConcurrentQueue<Message> Packets { get; }
+        bool Connected { get; set; }
+        void ClearPackets();
+        Queue<Message> GetPacketsLogsAndStop();
+        event Action<string> Warning;
+        void CleanupForcefully();
     }
 }
