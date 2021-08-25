5c8976c750e6f607ac868a8382d8c761dc67b4eb
Gl0 <Gl0@users.noreply.github.com>
2021-08-25 23:46:17 +0300

    Patch 110 ESR Update

--- Game/Messages/Server/EachSkillResultServerMessage.cs
+++ Game/Messages/Server/EachSkillResultServerMessage.cs
@@ -30,7 +30,10 @@ namespace Tera.Game.Messages
             TemplateId = reader.ReadInt32();
 
             SkillId = new SkillId(reader).Id;
-
+            if (reader.Factory.ReleaseVersion >= 11001) { //todo: override for 99.01 
+                var originalSkillId = new SkillId(reader).Id;
+                if (originalSkillId != 0) SkillId = originalSkillId; //not sure what is originalSkillId, but ingame meter use it in this way. todo: look at it later if they implement something new.
+            }
             HitId = reader.ReadInt32(); //index in TargetingList (NOT id) - See DataCenter.SkillData
             Unknow2 = reader.ReadBytes(12); //index in area, id, time
 
