1a472f13a1c41bc0e3cf62bd6d051db8a664b9e9
Gl0 <Gl0@users.noreply.github.com>
2021-09-29 03:30:10 +0300

    ignore originalSkillId introduced in p110

--- Game/Messages/Server/EachSkillResultServerMessage.cs
+++ Game/Messages/Server/EachSkillResultServerMessage.cs
@@ -32,7 +32,8 @@ namespace Tera.Game.Messages
             SkillId = new SkillId(reader).Id;
             if (reader.Factory.ReleaseVersion >= 11001 || reader.Factory.ReleaseVersion == 9901 ) {
                 var originalSkillId = new SkillId(reader).Id;
-                if (originalSkillId != 0) SkillId = originalSkillId; //not sure what is originalSkillId, but ingame meter use it in this way. todo: look at it later if they implement something new.
+                //if (originalSkillId != 0) SkillId = originalSkillId; //not sure what is originalSkillId, but ingame meter use it in this way. todo: look at it later if they implement something new.
+                //seems it contains base skill id to simplify official meter DC queries, not needed here.
             }
             HitId = reader.ReadInt32(); //index in TargetingList (NOT id) - See DataCenter.SkillData
             Unknow2 = reader.ReadBytes(12); //index in area, id, time
